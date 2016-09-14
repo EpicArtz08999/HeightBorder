3a2aef24c949fed65198c613bae7bb6cf5d2d71b
Blabla <st.ten.veldhuis@gmail.com>
2016-09-14 08:00:14 +0200

    Update Main.php

--- src/joincommands/Main.php
+++ src/joincommands/Main.php
@@ -10,7 +10,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
-use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\item\Item;
 use joincommands\LoginTask;
 use pocketmine\utils\TextFormat as TF;
@@ -26,7 +26,7 @@ class Main extends PluginBase implements Listener{
     $this->getServer()->getScheduler()->scheduleDelayedTask(new LoginTask($this, $p), 400);
   }
 
-  public function onHeld (PlayerItemHeldEvent $event) {
+  public function onHeld (PlayerJoinEvent $event) {
     $item = Item::get (175, 0, 1);
     $item->setCustomName("Coin");
     $item2 = Item::get (266, 0, 1);
