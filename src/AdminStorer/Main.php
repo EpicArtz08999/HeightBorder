d4034ad1a8148b27d61335ece7e409be21870ebc
Blabla <st.ten.veldhuis@gmail.com>
2016-10-06 13:04:33 +0200

    Update Main.php

--- src/AdminStorer/Main.php
+++ src/AdminStorer/Main.php
@@ -14,12 +14,22 @@ use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\item\Item;
 use joincommands\LoginTask;
 use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
 
   public function onEnable () {
     $this->getServer ()->getPluginManager ()->registerEvents ($this, $this);
     mkdir($this->getDataFolder());
-    
+    $this->cfg = new Config($this->getDataFolder() . "admins.yml");
+    $admins = scandir("plugins/PurePerms/players/");
+    foreach($admins as $admin) {
+      if($admin != "." && $admin != "..") {
+        $this->admin = new Config("plugins/PurePerms/players/");
+        if($this->admin->get("group") == "Admin") {
+         
+        }
+      }
+    }
   }
 }
