a123e7c888669c05eeb80b6f7acdd3df8573fba6
Blabla <st.ten.veldhuis@gmail.com>
2016-10-07 09:16:24 +0200

    Update Main.php

--- src/HeightBorder/Main.php
+++ src/HeightBorder/Main.php
@@ -5,14 +5,8 @@ namespace HeightBorder;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 use pocketmine\Server;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerLoginEvent;
-use pocketmine\event\player\PlayerItemHeldEvent;
-use pocketmine\item\Item;
-use joincommands\LoginTask;
+use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\utils\Config;
 
@@ -20,10 +14,16 @@ class Main extends PluginBase implements Listener{
 
   public function onEnable () {
     $this->getServer ()->getPluginManager ()->registerEvents ($this, $this);
+    mkdir($this->getDataFolder());
+    $this->saveResource("config.yml");
+    $this->cfg = new Config($this->getDataFolder(), "config.yml");
   }
 
   public function onMove(PlayerMoveEvent $event) {
     $p = $event->getPlayer();
     $l = $p->getLevel();
+    if(in_array($l->getName(), $this->cfg->get("Worlds", array()))) {
+      
+    }  
   }
 }
